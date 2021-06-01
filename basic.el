c9d4d346e366b9c3ad994bd4542a127dde77699c
leep-frog <gleeper49@gmail.com>
2021-06-01 19:49:44 -0400

    bug fixes

--- basic.el
+++ basic.el
@@ -11,7 +11,7 @@
 (global-set-key [(control ?h)] 'backward-kill-word)
 
 ;; un-swap c-f and c-s.
-(global-set-key (kbd "C-f" 'forward-char))
+(global-set-key (kbd "C-f") 'forward-char)
 (global-set-key (kbd "C-s") 'isearch-forward)
 ;; Switch C-f to find next occurrence when in search mode.
 (define-key isearch-mode-map (kbd "C-s") 'isearch-repeat-forward)
